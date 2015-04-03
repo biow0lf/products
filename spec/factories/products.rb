1e6ebda46286588828564c4011220faa3738741f
Igor Zubkov <igor.zubkov@gmail.com>
2015-04-03 22:25:58 +0300

    Add invalid product factory

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -2,5 +2,9 @@ FactoryGirl.define do
   factory :product do
     sequence(:name) { |n| "#{ Faker::Commerce.product_name } #{ n }" }
     description { Faker::Lorem.paragraph }
+
+    factory :product_invalid do
+      name nil
+    end
   end
 end
